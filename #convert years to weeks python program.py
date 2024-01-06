cef6ddf8c9903fe99b295a6ce91fdcbb856384b4
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-01-06 19:40:59 +0530

    added

--- #convert years to weeks python program.py
+++ #convert years to weeks python program.py
@@ -1,6 +1,10 @@
 #convert years to weeks python program
-def convert(year):
-    ans=year*52
-    return ans
-year=int(input("Enter the number of years: "))
-convert(year)
+def convert(in_year):
+    days_left=32850-(in_year*365)
+    year_left=90-in_year
+    months_left=year_left*12
+    weeks_left=year_left*52
+   
+    return f"years left on earth for u is {year_left} years {months_left} months ,{weeks_left} weeks and {days_left} days"
+in_year=int(input("Enter the number of years: "))
+print(convert(in_year))
