9cade3f6aac61228627c6ec6a4ba792e0b10da2a
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-01-06 20:44:19 +0530

    added

---
+++ #amstrong number.py
@@ -0,0 +1,14 @@
+#amstrong number
+for i in range(1001):
+    num=i
+    sum=0
+    n=len(str(i))
+    while i!=0:
+       
+        digit=i%10
+        sum=sum+digit**n
+        i=i//10
+        if num==sum:
+            print("amstrong number is",num)
+        
+    
