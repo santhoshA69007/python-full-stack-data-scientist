9e084a6e01fbd1db9ce00bfcc5e6ce255eff7ff1
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-01-07 00:58:07 +0530

    added

--- #hollow right angle triangle pattern pro.py
+++ #hollow right angle triangle pattern pro.py
@@ -2,8 +2,8 @@
 n=int(input("enter the no of rows:"))
 for row in range(n):
     for col in range(n):
-        if col==0 or row==n-1 or row==col:
+        if col==0 or row==(n-1) or row==col:
             print("*",end="")
         else:
-            print(end="")
+            print(end=" ")
     print()
