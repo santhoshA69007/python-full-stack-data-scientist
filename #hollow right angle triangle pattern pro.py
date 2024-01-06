263edea33cd0adee318d8d9273a60274fdfdd5b5
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-01-06 23:31:57 +0530

    created

---
+++ #hollow right angle triangle pattern pro.py
@@ -0,0 +1,9 @@
+#hollow right angle triangle pattern program
+n=int(input("enter the no of rows:"))
+for row in range(n):
+    for col in range(n):
+        if col==0 or row==n-1 or row==col:
+            print("*",end="")
+        else:
+            print(end="")
+    print()
--- #right_triangle.py
+++ #right_triangle.py
@@ -4,3 +4,10 @@ for i in range(1,n+1):
     for j in range(1,i+1):
         print(j,end=" ")
     print()
+    
+#pattern program -1 same name in each row
+n=int(input("enter the number of rows"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i,end=" ")
+    print()
