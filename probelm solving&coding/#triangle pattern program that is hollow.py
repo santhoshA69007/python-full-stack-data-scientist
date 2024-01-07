ded6f7ae5c64b02c55eff6e8d2f2cb0425f8449c
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-01-07 19:07:12 +0530

    added

--- probelm solving&coding/#triangle pattern program that is hollow.py
+++ probelm solving&coding/#triangle pattern program that is hollow.py
@@ -7,3 +7,13 @@ for row in range(1,5):
         else:
             print(end=" ")
     print()
+#triangle pattern program that is hollow inside with user input
+n=int(input("enter the number of rows"))
+print()
+for row in range(1,n+1):
+    for col in range(1,2*n):
+        if row==n or row+col==n+1 or col-row==n-1:
+             print("*",end="")
+        else:
+            print(end=" ")
+    print()
