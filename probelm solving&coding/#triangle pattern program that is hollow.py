713e48cf3108fadc27c08719330bf0d4e4117e21
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-01-07 19:39:06 +0530

    created

--- probelm solving&coding/#triangle pattern program that is hollow.py
+++ probelm solving&coding/#triangle pattern program that is hollow.py
@@ -17,3 +17,19 @@ for row in range(1,n+1):
         else:
             print(end=" ")
     print()
+#type 3 triangle bottom pattern stars are even
+n=int(input("enter the number of rows"))
+k=2
+print()
+for row in range(1,n+1):
+    for col in range(1,2*n):
+        if row+col==n+1 or col-row==n-1:
+             print("*",end="")
+        elif row==n and col!=k:
+             print("*",end="")
+             k+=2
+             
+        
+        else:
+            print(end=" ")
+    print()
